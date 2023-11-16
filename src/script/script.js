597c779b93e2e9a3771b0bb499fb3acf2f1e7408
Kristina Dudjak <dudjakkristina@gmail.com>
2023-11-16 15:08:47 +0100

    feat: implement header

---
+++ src/script/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../../header.html').then(response => response.text()).then(data => {
+        document.getElementById('header').innerHTML=data;
+    })
+    .catch(error => console.error('Error fetching header:', error));
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'titillium': ['Titillium Web'],
+        'bebas': ['Bebas Neue']
+      },
+      boxShadow: {
+        'md': '0 4px 24px 0 rgba(0, 0, 0, 0.16)'
+      }
+    },
+    colors: {
+      gray: "#828282",
+      black: "#2F2F2F",
+      white: "#F5F5F5",
+      orange: "#E0872C",
+      green: "#008421"
+    },
   },
   plugins: [],
-}
+};
 
