97754bb6a2f481df93175663b301df340194f7fd
Kristina Dudjak <dudjakkristina@gmail.com>
2023-11-19 17:10:41 +0100

    feat: add shopping cart items, footer

--- src/script/script.js
+++ src/script/script.js
@@ -4,3 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(error => console.error('Error fetching header:', error));
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../../cart-item.html').then(response => response.text()).then(data => {
+        document.getElementById('cart-item-1').innerHTML=data;
+    })
+    .catch(error => console.error('Error fetching cart item:', error));
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../../cart-item.html').then(response => response.text()).then(data => {
+        document.getElementById('cart-item-2').innerHTML=data;
+    })
+    .catch(error => console.error('Error fetching cart item:', error));
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../../footer.html').then(response => response.text()).then(data => {
+        document.getElementById('footer').innerHTML=data;
+    })
+    .catch(error => console.error('Error fetching footer:', error));
+})
