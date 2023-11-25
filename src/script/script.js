6e76c82465372ef703976ed5c6e2c7f1ce81d86c
Kristina Dudjak <dudjakkristina@gmail.com>
2023-11-25 01:49:01 +0100

    feat: add responsive breakpoints

--- src/script/script.js
+++ src/script/script.js
@@ -1,52 +1,35 @@
+async function fetchTemplate(url, elementId) {
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        document.getElementById(elementId).innerHTML = data;
+    } catch (error) {
+        console.error(`Error fetching ${elementId}:`, error);
+    }
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../header.html').then(response => response.text()).then(data => {
-        document.getElementById('header').innerHTML=data;
-    })
-    .catch(error => console.error('Error fetching header:', error));
-})
+document.addEventListener('DOMContentLoaded', async function () {
+    await Promise.all([
+        fetchTemplate('../../header.html', 'header'),
+        fetchTemplate('../../cart-item.html', 'cart-item-1'),
+        fetchTemplate('../../cart-item.html', 'cart-item-2'),
+        fetchTemplate('../../footer.html', 'footer'),
+        fetchTemplate('../../summary.html', 'summary')
+    ]);
+    const shippingBox = document.getElementById('shipping-box');
+    const shippingInfo = document.getElementById('shipping-info');
+    const shippingText = document.getElementById('shipping-text');
+    const expand = document.getElementById('expand');
+    const hide = document.getElementById('hide');
+    const footer = document.getElementById('footer');
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../cart-item.html').then(response => response.text()).then(data => {
-        document.getElementById('cart-item-1').innerHTML=data;
-    })
-    .catch(error => console.error('Error fetching cart item:', error));
-})
+    shippingBox.addEventListener('click', function () {
+        const isHidden = shippingInfo.classList.toggle('hidden');
+        footer.style.paddingTop = isHidden ? '0' : '182px';
+        shippingText.style.color = isHidden ? '#2F2F2F' : '#008421';
+        expand.style.display = isHidden ? 'inline' : 'none';
+        hide.style.display = isHidden ? 'none' : 'inline';
+        hide.style.color = isHidden ? '' : '#008421';
+    });
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../cart-item.html').then(response => response.text()).then(data => {
-        document.getElementById('cart-item-2').innerHTML=data;
-    })
-    .catch(error => console.error('Error fetching cart item:', error));
-})
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../footer.html').then(response => response.text()).then(data => {
-        document.getElementById('footer').innerHTML=data;
-    })
-    .catch(error => console.error('Error fetching footer:', error));
-})
-
-document.addEventListener('DOMContentLoaded', function () {  
-    fetch('../../summary.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('summary').innerHTML = data;
-        const shippingBox = document.getElementById('shipping-box');
-        const shippingInfo = document.getElementById('shipping-info');
-        const shippingText = document.getElementById('shipping-text');
-        const expand = document.getElementById('expand');
-        const hide = document.getElementById('hide');
-        const footer = document.getElementById('footer');
-  
-        shippingBox.addEventListener('click', function () {
-          const isHidden = shippingInfo.classList.toggle('hidden');
-          footer.style.paddingTop = isHidden ? '0' : '182px';
-          shippingText.style.color = isHidden ? '#2F2F2F' : '#008421';
-          expand.style.display = isHidden ? 'inline' : 'none';
-          hide.style.display = isHidden ? 'none' : 'inline';
-          hide.style.color = isHidden ? '' : '#008421';
-        });
-      })
-      .catch(error => console.error('Error fetching summary:', error));
-  });
